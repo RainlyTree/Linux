cb9016ced5beb4e38a6ad7cbeefd9bc5f308b019
RainlyTree <a1123845350@163.com>
2020-02-19 17:55:26 +0800

    fork_study

---
+++ test/test_pid/fork_test.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<unistd.h>
+
+int main()
+{
+    int ret = fork();
+    if(ret < 0)
+    {
+        perror("fork");
+        return 1;
+    }
+    else if (ret == 0)
+    {
+        printf("i am child :%d, ret : %d\n",getpid(), ret);
+    }
+    else
+    {
+
+        printf("i am father :%d, ret : %d",getpid(), ret);
+    }
+    return 0;
+}
