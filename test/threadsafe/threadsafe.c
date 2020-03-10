e0aa1ca0ad4013b55352a2ebe6739637e0955601
RainlyTree <a1123845350@163.com>
2020-03-10 17:41:45 +0800

    cond

--- test/threadsafe/threadsafe.c
+++ test/threadsafe/threadsafe.c
@@ -6,23 +6,26 @@
 
 #define  THREADCOUNT 4
 int g_tick = 100;
+pthread_mutex_t mutex;
 
 void* thread_start(void* arg)
 {
     (void)arg;
     while(1)
     {
+        pthread_mutex_lock(&mutex);
         if(g_tick > 0)
         {
+            printf("i am thread [%d],i get ticket [%d]\n", *(int*)arg, g_tick); 
             g_tick = g_tick - 1;
-            printf("i am thread [%d],i get ticket [%d]\n", pthread_self(), g_tick);
-            
-            usleep(1000);
+            sleep(1);
         }
         else
         {
+            pthread_mutex_unlock(&mutex);
             break;
         }
+        pthread_mutex_unlock(&mutex);
     }
     return NULL;
 }
@@ -31,9 +34,10 @@ int main()
 {
     pthread_t tid[THREADCOUNT];
     int i = 0;
+    pthread_mutex_init(&mutex, NULL);
     for(; i < THREADCOUNT; ++i)
     {
-        pthread_create(&tid[i], NULL, thread_start, NULL);
+        pthread_create(&tid[i], NULL, thread_start, (void*)&i);
     }
 
     for(i = 0; i < THREADCOUNT; ++i)
@@ -41,5 +45,6 @@ int main()
         pthread_join(tid[i],NULL);
 
     }
+    pthread_mutex_destroy(&mutex);
     return 0;
 }
